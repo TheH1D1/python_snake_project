a9d9edc7662267555cd373fd8198af60e5d16dd0
Hidi <87263099+TheH1D1@users.noreply.github.com>
2024-12-16 11:06:15 +0100

    Apple
    apple.py - Apples random spawn in the scene

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from settings import *
 
 from snake import Snake
+from apple import Apple
 
 class Main:
     def __init__(self):
@@ -15,6 +16,7 @@ class Main:
         self.bg_rects = [pygame.Rect((col + int(row % 2 == 0)) * CELL_SIZE, row * CELL_SIZE, CELL_SIZE, CELL_SIZE) for col in range(0,COLS,2) for row in range(ROWS)]
 
         self.snake = Snake()
+        self.apple = Apple(self.snake)
 
         
 
@@ -34,6 +36,7 @@ class Main:
             
             self.drawbg()
             self.snake.draw()
+            self.apple.draw()
             pygame.display.update()
 
 if __name__ == '__main__':
--- snake.py
+++ snake.py
@@ -4,5 +4,8 @@ class Snake:
     def __init__(self):
         self.display_surface = pygame.display.get_surface()
         self.body = [pygame.Vector2(START_COL - col,START_ROW) for col in range(START_LENGHT)]
-        print(self.body)
         
+    def draw(self):
+        for point in self.body:
+            rect = pygame.Rect(point.x * CELL_SIZE,point.y * CELL_SIZE, CELL_SIZE,CELL_SIZE)
+            pygame.draw.rect(self.display_surface, 'red',rect)
