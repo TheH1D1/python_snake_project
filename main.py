6c0457c3a40dfdc41da6e5957be93885ccb31ef4
Hidi <87263099+TheH1D1@users.noreply.github.com>
2024-12-16 22:31:54 +0100

    Update
    Snake now can move, eat apple and if the snake head collide with the body or with the end of the window the snake should get back to the original point.

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from settings import *
 
 from snake import Snake
-from apple import Apple
 
 class Main:
     def __init__(self):
@@ -16,7 +15,6 @@ class Main:
         self.bg_rects = [pygame.Rect((col + int(row % 2 == 0)) * CELL_SIZE, row * CELL_SIZE, CELL_SIZE, CELL_SIZE) for col in range(0,COLS,2) for row in range(ROWS)]
 
         self.snake = Snake()
-        self.apple = Apple(self.snake)
 
         
 
@@ -36,7 +34,6 @@ class Main:
             
             self.drawbg()
             self.snake.draw()
-            self.apple.draw()
             pygame.display.update()
 
 if __name__ == '__main__':
